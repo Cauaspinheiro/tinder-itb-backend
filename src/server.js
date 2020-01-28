4d1b7516194008d0406afe26d821b2914531b41c
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-01-28 19:44:01 -0300

    adiciona conexao com mongodb usando mongoose

--- src/server.js
+++ src/server.js
@@ -1,3 +1,11 @@
+import dotenv from "dotenv";
 import app from "./App";
+import mongoose from "mongoose";
+
+dotenv.config();
+mongoose.connect(process.env.MONGO_KEY, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 app.listen(3001);
