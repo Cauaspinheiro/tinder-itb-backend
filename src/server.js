b2f6ab05a7db7b52ff59c33cc03a2bf9c45d50d7
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-01-30 11:57:58 -0300

    adiciona UseFindAndModify pra falso a pedido do mongoose

--- src/server.js
+++ src/server.js
@@ -5,7 +5,8 @@ import mongoose from "mongoose";
 dotenv.config();
 mongoose.connect(process.env.MONGO_KEY, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  useFindAndModify: false
 });
 
 app.listen(3001);
