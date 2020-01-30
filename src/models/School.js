3215162e3e77e802027ddb305aecc77af53de74b
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-01-30 12:51:23 -0300

    Adiciona document School

---
+++ src/models/School.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const SchoolSchema = new mongoose.Schema(
+  {
+    nome: {
+      type: String,
+      required: true
+    },
+    cursos: {
+      type: [String],
+      required: true
+    }
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("School", SchoolSchema);
