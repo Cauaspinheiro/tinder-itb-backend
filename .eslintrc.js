85a77e7578428364180a9b5865480be22d955278
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-01-31 14:59:17 -0300

    chore: adiciona package import helper para padronização de imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,9 +14,23 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
+  plugins: [
+    "import-helpers",
+  ],
   rules: {
     "no-param-reassign": "off",
     "camelcase": "off",
-    "no-unused-vars": ["error", {"argsIgnorePattern": "next"}]
+    "no-unused-vars": ["error", {"argsIgnorePattern": "next"}],
+    'import-helpers/order-imports': [
+      'warn',
+      {
+          newlinesBetween: 'always', // new line between groups
+          groups: [
+              'module',
+              ['parent', 'sibling', 'index'],
+          ],
+          alphabetize: { order: 'asc', ignoreCase: true },
+      },
+  ],
   },
 };
