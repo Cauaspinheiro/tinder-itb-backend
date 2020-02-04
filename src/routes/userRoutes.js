5f64dd5625dd567bfd5b95ff00a6e366a1ccca0b
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-02-04 19:44:54 -0300

    feat: adiciona checkagem de auth em rotas Users
    Além de apenas checkar se o token é valido, também checka se o id do token é o mesmo que está
    querendo acessa o usuário do req.params
    
    BREAKING CHANGE: agora é preciso de um token para acessar as rotas User, com exceção da rota post

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -9,7 +9,10 @@ import checkInfo from '../middlewares/User/info';
 import notUser from '../middlewares/User/notUser';
 import passwordHash from '../middlewares/User/password';
 import getPrefs from '../middlewares/User/prefs';
+import sameUser from '../middlewares/User/sameUser';
 import searchParams from '../middlewares/User/search';
+import contactUpdate from '../middlewares/User/updateInfo/contact';
+import passwordUpdate from '../middlewares/User/updateInfo/password';
 
 export default (routes) => {
   routes.get('/users', auth,
@@ -27,18 +30,20 @@ export default (routes) => {
     UserController.store);
 
 
-  routes.get('/users/:id',
+  routes.get('/users/:id', auth,
     getUser,
     UserController.show);
 
-  routes.put('/users/:id',
+  routes.put('/users/:id', auth, sameUser,
     getUser,
+    passwordUpdate,
     passwordHash,
+    contactUpdate,
     getSchool,
     getCourse,
     UserController.update);
 
-  routes.delete('/users/:id',
+  routes.delete('/users/:id', auth, sameUser,
     getUser,
     UserController.destroy);
 };
