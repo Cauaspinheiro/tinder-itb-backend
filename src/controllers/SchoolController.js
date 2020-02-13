267fe140b5bb545fa97029fe61d85009b06c319f
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-02-13 16:52:22 -0300

    fix: conserta http status em SchoolController

--- src/controllers/SchoolController.js
+++ src/controllers/SchoolController.js
@@ -12,9 +12,9 @@ export default {
 
     const school = await School.findOne({ _id });
 
-    if (!school) return res.json('School not found');
+    if (!school) return res.status(404).json('School not found');
 
-    return res.status(201).json(school);
+    return res.status(200).json(school);
   },
 
   async store(req, res) {
@@ -22,11 +22,11 @@ export default {
 
     let school = await School.findOne({ nome });
 
-    if (school) return res.json('Escola já existe');
+    if (school) return res.status(400).json('Escola já existe');
 
     school = await School.create(req.body);
 
-    return res.status(200).json(school);
+    return res.status(201).json(school);
   },
 
   async update(req, res) {
@@ -34,7 +34,7 @@ export default {
 
     let school = await School.findOne({ _id });
 
-    if (!school) return res.json('School not found');
+    if (!school) return res.status(404).json('School not found');
 
     school = await School.findOneAndUpdate({ _id }, req.body, { new: true });
 
@@ -46,7 +46,7 @@ export default {
 
     let school = await School.findOne({ _id });
 
-    if (!school) return res.json('School not found');
+    if (!school) return res.status(404).json('School not found');
 
     school = await School.findOneAndDelete({ _id });
 
