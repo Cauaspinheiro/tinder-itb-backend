5782b8d2a8ec06851a4d6efe50b4e178607df398
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-02-21 18:22:04 -0300

    perf: deleta middlewares desnecessários

--- src/middlewares/Global/sameUser.js
+++
@@ -1,8 +0,0 @@
-export default (req, res, next) => {
-  const { id: authId } = req;
-  const { id: paramId } = req.params;
-
-  if (authId === paramId) return next();
-
-  return res.status(401).json({ error: 'NOT AUTHORIZED' });
-};
