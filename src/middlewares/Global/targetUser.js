0b303d52bced08d035cb37567222635fb5e896d7
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-02-21 18:23:15 -0300

    perf: padroniza middlewares para as novas rotas

--- src/middlewares/Global/targetUser.js
+++ src/middlewares/Global/targetUser.js
@@ -1,11 +1,11 @@
 import User from '../../models/User';
 
 export default async (req, res, next) => {
-  const { id: _id } = req.headers;
+  const { id: _id } = req.params;
 
   const user = await User.findOne({ _id });
 
-  if (!user) return res.status(404).json({ error: 'USER NOT FOUND (HEADER)' });
+  if (!user) return res.status(404).json({ error: 'TARGET USER NOT FOUND' });
 
   req.targetUser = user;
 
--- src/middlewares/Global/user.js
+++ src/middlewares/Global/user.js
@@ -1,11 +1,11 @@
 import User from '../../models/User';
 
 export default async (req, res, next) => {
-  const { id: _id } = req.params;
+  const { id: _id } = req;
 
   const user = await User.findOne({ _id });
 
-  if (!user) return res.status(404).json({ error: 'USER NOT FOUND (PARAMS)' });
+  if (!user) return res.status(404).json({ error: 'USER NOT FOUND' });
 
   req.user = user;
 
