2c348420f7595ad9945b21f907134bd62798c4ae
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-02-25 14:47:12 -0300

    feat: cria arquivo de configuracao do multer

---
+++ src/config/multer.js
@@ -0,0 +1,39 @@
+import crypto from 'crypto';
+import multer from 'multer';
+import path from 'path';
+
+export default {
+  dest: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, path.resolve(__dirname, '..', '..', 'tmp', 'uploads'));
+    },
+    filename: (req, file, cb) => {
+      crypto.randomBytes(12, (err, hash) => {
+        if (err) cb(err);
+
+        const filename = `${hash.toString('hex')}-${Date.now()}-${file.originalname}`;
+
+        cb(null, filename);
+      });
+    },
+  }),
+  limits: {
+    fileSize: 2 * 1024 * 1024,
+  },
+  fileFilter: (req, file, cb) => {
+    const allowedMimes = [
+      'image/jpeg',
+      'image/pjpeg',
+      'image/png',
+      'image/gif',
+    ];
+
+
+    if (allowedMimes.includes(file.mimetype)) {
+      cb(null, true);
+    } else {
+      cb(new Error('INVALID FILE TYPE'));
+    }
+  },
+};
