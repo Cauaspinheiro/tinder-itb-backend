2ceedcbc7ef70c74e00541a520ad2ef15cc6ae8a
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-02-26 11:14:58 -0300

    refactor: retira as rotas que nao estao disponiveis
    agora em users ficarao apenas as rotas INDEX e SHOW
    
    BREAKING CHANGE: para acessar as rotas retiradas use /profile

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,17 +1,10 @@
 import UserController from '../controllers/UserController';
 
 import auth from '../middlewares/Global/auth';
-import getSchool from '../middlewares/Global/school';
 import getUser from '../middlewares/Global/user';
-import getCourse from '../middlewares/User/course';
-import checkInfo from '../middlewares/User/info';
-import notUser from '../middlewares/User/notUser';
-import passwordHash from '../middlewares/User/password';
 import getPrefs from '../middlewares/User/prefs';
 import searchParams from '../middlewares/User/search';
-import contactUpdate from '../middlewares/User/updateInfo/contact';
-import courseUpdate from '../middlewares/User/updateInfo/course';
-import passwordUpdate from '../middlewares/User/updateInfo/password';
+
 
 export default (routes) => {
   routes.get('/users', auth,
@@ -20,29 +13,7 @@ export default (routes) => {
     getPrefs,
     UserController.index);
 
-  routes.post('/users',
-    notUser,
-    checkInfo,
-    passwordHash,
-    getSchool,
-    getCourse,
-    UserController.store);
-
-
   routes.get('/users/show', auth,
     getUser,
     UserController.show);
-
-  routes.put('/users', auth,
-    getUser,
-    passwordUpdate,
-    passwordHash,
-    contactUpdate,
-    getSchool,
-    courseUpdate,
-    UserController.update);
-
-  routes.delete('/users', auth,
-    getUser,
-    UserController.destroy);
 };
