bac2de59d1df5670b290cf3756436099eb7db3ea
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-04-02 09:53:49 -0300

    fix(errors/sentry): retorna corretamente erro em JSON de retorno

--- src/errors/sentry.js
+++ src/errors/sentry.js
@@ -3,5 +3,5 @@ import * as Sentry from '@sentry/node';
 export default (err, req, res, next) => {
   Sentry.captureException(err);
 
-  return res.status(500).json({ error: 'SOMETHING WENT WRONG', details: err });
+  return res.status(500).json({ error: 'SOMETHING WENT WRONG', details: err.toString() });
 };
