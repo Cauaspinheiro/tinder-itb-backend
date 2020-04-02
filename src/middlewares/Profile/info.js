6795c779bef73c462d9ddd0fd9a95ece476760b3
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-04-02 10:02:36 -0300

    refactor(middlewares/profile): retira tratamento de inputs desnecessário

--- src/middlewares/Profile/info.js
+++ src/middlewares/Profile/info.js
@@ -1,17 +1,9 @@
 export default (req, res, next) => {
-  const { contatos, password } = req.body;
-
-  if (!contatos) return res.status(400).json({ error: 'CONTACTS NOT FOUND' });
-
-  const { numero } = contatos;
+  const { numero } = req.body.contatos;
 
   if (numero) {
     if (numero.length !== 13) return res.status(400).json({ error: 'INVALID PHONE NUMBER' });
   }
-
-  if (!password) return res.status(400).json({ error: 'PASSWORD NOT FOUND' });
-
-  if (password.length < 6) return res.status(400).json({ error: 'INVALID PASSWORD' });
 
   req.body.show_me = true;
 
