43c1df1c3a83ceb32c7ab71656106654e476a472
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-04-03 10:54:33 -0300

    feat(middlewares/profile/info): adiciona validação de numero e twitter

--- src/middlewares/Profile/info.js
+++ src/middlewares/Profile/info.js
@@ -1,8 +1,14 @@
 export default (req, res, next) => {
-  const { numero } = req.body.contatos;
+  const { numero, twitter } = req.body.contatos;
 
   if (numero) {
     if (numero.length !== 13) return res.status(400).json({ error: 'INVALID PHONE NUMBER' });
+
+    if (numero.indexOf('5511') !== 0) return res.status(400).json({ error: 'INVALID PHONE NUMBER' });
+  }
+
+  if (twitter) {
+    if (twitter.indexOf('@') !== 0) return res.status(400).json({ error: 'INVALID TWITTER USERNAME' });
   }
 
   req.body.show_me = true;
