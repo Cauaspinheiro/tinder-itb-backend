ba69ff524261b40788031177ddadc84f903e9bdd
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-04-22 19:58:34 -0300

    feat(routes/session): adiciona rota para validação
    ENDPOINT: GET /sessions

--- src/routes/sessionRoutes.js
+++ src/routes/sessionRoutes.js
@@ -2,11 +2,13 @@ import { Router } from 'express';
 
 import SessionController from '../controllers/SessionController';
 
+import validate from '../middlewares/Global/auth';
 import email from '../middlewares/Session/email';
 import password from '../middlewares/Session/password';
 
 const routes = Router();
 
 routes.post('/sessions', email, password, SessionController.store);
+routes.get('/sessions', validate, (req, res) => res.status(204));
 
 export default routes;
