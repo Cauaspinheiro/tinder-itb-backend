9c9074a5e6607d7682efdd168f32093674ab2b6a
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-04-22 22:27:10 -0300

    feat(routes/session): adiciona verificação se usuário existe em GET

--- src/routes/sessionRoutes.js
+++ src/routes/sessionRoutes.js
@@ -3,12 +3,13 @@ import { Router } from 'express';
 import SessionController from '../controllers/SessionController';
 
 import validate from '../middlewares/Global/auth';
+import user from '../middlewares/Global/user';
 import email from '../middlewares/Session/email';
 import password from '../middlewares/Session/password';
 
 const routes = Router();
 
 routes.post('/sessions', email, password, SessionController.store);
-routes.get('/sessions', validate, (req, res) => res.status(204).end());
+routes.get('/sessions', validate, user, (req, res) => res.status(204).end());
 
 export default routes;
