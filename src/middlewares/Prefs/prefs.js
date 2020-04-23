60e7897193862b02144eb7b4ec7be9191fa8b998
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-04-23 20:20:14 -0300

    refactor(prefs): retira todos os arquivos de prefs
    essa features agora ficará dentro de profile

--- src/middlewares/Prefs/prefs.js
+++
@@ -1,9 +0,0 @@
-export default (req, res, next) => {
-  const { prefs } = req.user;
-
-  if (!prefs) return res.status(404).json({ error: 'PREFS NOT FOUND' });
-
-  req.prefs = prefs;
-
-  return next();
-};
