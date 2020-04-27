44e4651b336e7167bf2d564a6f58e4792034a223
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-04-27 12:05:42 -0300

    refactor(erros/parsejson): usa novo padrão de mensagem de erro

--- src/errors/parseJson.js
+++ src/errors/parseJson.js
@@ -1,6 +1,30 @@
 export default (err, req, res, next) => {
   if (err instanceof SyntaxError) {
-    return res.status(400).json({ error: 'JSON NOT VALID', details: err.message });
+    return res.status(400).json({
+      status: '400 Bad Request',
+      error: {
+        en_us: 'JSON NOT VALID',
+        pt_br: 'JSON NÃO É VALIDO',
+      },
+      details: {
+        en_us: 'JSON syntax is invalid, check for any errors in your JSON. '
+        + 'For more info about JSON: '
+        + 'https://www.w3schools.com/js/js_json_syntax.asp',
+        pt_br: 'A sintaxe do JSON não é valida, verifique se o JSON enviado tem'
+        + ' sua sintaxe válida. '
+        + 'Para mais informações sobre o JSON, acesse '
+        + 'https://dicasdeprogramacao.com.br/o-que-e-json/',
+      },
+      type: SyntaxError,
+      more_info: {
+        syntax_error: err.message,
+      },
+      full_error: {
+        message: err.message,
+        name: err.name,
+        stack: err.stack,
+      },
+    });
   }
 
   return next();
