cd3260706d935e1c7e8bf0739723e4dbfba074bd
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-04-27 21:31:06 -0300

    refactor(errors/notroute): padroniza retorno de erro

--- src/errors/notRoute.js
+++ src/errors/notRoute.js
@@ -1,3 +1,19 @@
 export default (req, res, next) => res.status(404).json({
-  error: `CANNOT ${req.method} IN ${req.url}`,
+  status: '404 Not Found',
+  error: {
+    en_us: `CANNOT ${req.method} IN ${req.url}`,
+    pt_br: `NÃO FOI POSSÍVEL ${req.method} EM ${req.url}`,
+  },
+  details: {
+    en_us: `Cannot find the method ${req.method} in the route ${req.url}, `
+    + 'check if the route and the method are both correct',
+    pt_br: `Não encontramos o método ${req.method} na rota ${req.url}, verifique `
+    + 'se o caminho e o método estão corretos',
+  },
+  type: 'RouteError',
+  more_info: {
+    method: req.method,
+    route: req.url,
+  },
+  full_error: `CANNOT ${req.method} IN ${req.url}`,
 });
