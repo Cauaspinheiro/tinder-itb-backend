268cd1ec62842104f498c0712c70e94fb570c34d
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-04-27 21:05:45 -0300

    refactor(errors/id): retorna respostas de erro padronizada

--- src/errors/id.js
+++ src/errors/id.js
@@ -2,7 +2,30 @@ export default (err, req, res, next) => {
   if (!err.kind) return next(err);
 
   if (err.kind.toUpperCase() === 'OBJECTID') {
-    return res.status(400).json({ error: 'INVALID ID', id: err.value, model: err.model.modelName });
+    return res.status(400).json({
+      status: '400 Bad Request',
+      error: {
+        en_us: 'INVALID ID',
+        pt_br: 'ID INVÁLIDO',
+      },
+      details: {
+        en_us: `Invalid id ${err.value} for search data in database, because it`
+        + ' dont follow the required specifications. '
+        + 'For more info go to '
+        + 'https://docs.mongodb.com/manual/reference/bson-types/#objectid',
+        pt_br: `Não é possível usar o id ${err.value} para buscar dados no `
+        + 'banco de dados, pois ele não segue as especificações necessárias. '
+        + 'Para mais informações acesse '
+        + 'https://docs.mongodb.com/manual/reference/bson-types/#objectid '
+        + '(página em inglês)',
+      },
+      type: err.name,
+      more_info: {
+        model: err.model.modelName,
+        id: err.value,
+      },
+      full_error: err,
+    });
   }
 
   return next(err);
