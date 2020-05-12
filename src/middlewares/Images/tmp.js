fadfe061d6ed64712d1562f2b5674b37360ef40a
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-05-11 22:41:07 -0300

    refactor(middlewares/images/tmp): retira tratamento de erro
    tratamento de erro não era mais necessário

--- src/middlewares/Images/tmp.js
+++ src/middlewares/Images/tmp.js
@@ -1,23 +1,8 @@
 import fs from 'fs';
 import path from 'path';
 
-import errorByStatus from '../../errors/errorByStatus';
-
 export default (req, res, next) => {
   const { filename } = req.file;
-
-  if (!filename) {
-    errorByStatus(res, 404, {
-      error: {
-        pt_br: 'ARQUIVO NÃO ENCONTRADO',
-        en_us: 'FILE NOT FOUND',
-      },
-      details: {
-        pt_br: 'Nenhum arquivo foi encontrado, verifique se você está enviando o arquivo de forma correta.',
-        en_us: 'File not found, please check if you are sending the file.',
-      },
-    });
-  }
 
   fs.unlink(path.resolve(__dirname, '..', '..', '..', 'tmp', 'uploads', filename),
     (err) => {
